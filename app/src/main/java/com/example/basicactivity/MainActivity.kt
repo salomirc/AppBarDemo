a60458b08a3341b7e76785157378f6396b84c316
salox <ciprian.salomir@gmail.com>
2020-01-26 19:46:57 +0200

    Menu added.

--- app/src/main/java/com/example/basicactivity/MainActivity.kt
+++ app/src/main/java/com/example/basicactivity/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.example.basicactivity
 
+import android.app.SearchManager
+import android.content.Context
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-
+import android.widget.SearchView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 
+
 class MainActivity : AppCompatActivity() {
+
+    private var isFavSelected: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +31,35 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
+
+        //getting the search view from the menu
+        val searchViewItem = menu.findItem(R.id.menuSearch)
+
+        //getting search manager from systemservice
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+
+        //getting the search view
+        val searchView = searchViewItem.actionView as SearchView
+
+        //you can put a hint for the search input field
+        searchView.queryHint = "Search Tutorials..."
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+
+        //here we will get the search query
+        searchView.setOnQueryTextListener(object :
+            SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                //do the search here
+                Toast.makeText(this@MainActivity, "You are searching : $query", Toast.LENGTH_SHORT).show()
+                searchView.onActionViewCollapsed()
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
+            }
+        })
+
         return true
     }
 
@@ -33,9 +68,25 @@ class MainActivity : AppCompatActivity() {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_about -> true
-            R.id.action_settings -> true
-            R.id.action_logout -> true
+            R.id.action_favorite -> {
+                Toast.makeText(this, "You clicked favorite", Toast.LENGTH_SHORT).show()
+                isFavSelected = !isFavSelected
+                item.setIcon(if (isFavSelected) R.drawable.ic_favorite_red_24dp else R.drawable.ic_favorite_border_red_24dp)
+                true
+            }
+            R.id.action_about -> {
+                Toast.makeText(this, "You clicked about", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.action_settings -> {
+                Toast.makeText(this, "You clicked settings", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.action_logout -> {
+                Toast.makeText(this, "You clicked logout", Toast.LENGTH_SHORT).show()
+                true
+            }
+
             else -> super.onOptionsItemSelected(item)
         }
     }
